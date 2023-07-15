cdf1fed2b58162bcb172e78a63a2ef4233f00678
dastanasetullaev05 <legendaloukik@gmail.com>
2023-07-15 12:18:15 +0300

    dz-4

---
+++ main.js
@@ -0,0 +1,17 @@
+function findSmallerNumber(num1, num2) {
+    if (num1 < num2) {
+      return num1;
+    } else {
+      return num2;
+    }
+  }
+  
+  const result = findSmallerNumber(5, 10);
+  
+  console.log(result); 
+  function power(base, exponent) {
+    return Math.pow(base, exponent);
+  }
+  
+  const resul = power(2, 3);
+  console.log(resul);
